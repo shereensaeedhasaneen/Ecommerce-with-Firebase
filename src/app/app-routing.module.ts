8bafaf62709575e583a685b2240d839997b85a64
shereensaeedhasaneen <sheerysaeed123@gmail.com>
2022-09-06 16:02:33 +0200

    create project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { SignupComponent } from './components/signup/signup.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { GoodsComponent } from './components/goods/goods.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component:HomeComponent},
+  {path:'login' , component:LoginComponent},
+  {path:'signup' , component:SignupComponent},
+  {path:'cart' , component:CartComponent},
+  {path:'admin' , component:GoodsComponent},
+  {path:'**' , component:NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
